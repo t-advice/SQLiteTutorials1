6f8507d342ae683d17ce0824b54a89ab38056bcd
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-13 14:37:54 +0200

    delete function added  - right click

--- SQLiteTutorials1/MainPage.xaml.cs
+++ SQLiteTutorials1/MainPage.xaml.cs
@@ -33,8 +33,22 @@ namespace SQLiteTutorials1
             }
         
         }
+        private async void OnDeleteNoteClicked(object sender, EventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            var noteToDelete = menuItem?.CommandParameter as Note;
+            if (noteToDelete != null)
+            { 
+                bool confirm = await DisplayAlert("Delete", $"Are you sure you want to delete this note?", "Yes", "No");
+                if (confirm)
+                {
+                    await App.Database.DeleteNoteAsync(noteToDelete);
+                    LoadNotes(); // Refresh the list
+                }
+            }
+        }
 
 
-        
+
     }
 }
