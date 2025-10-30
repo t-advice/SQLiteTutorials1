7d0bf948e1edf53c46e7566b7ffb235169d584e2
Tashwill Advice <tashwilladvice02@gmail.com>
2025-10-30 20:16:24 +0200

    tutorial comments added to Note.cs model class

--- SQLiteTutorials1/Models/Note.cs
+++ SQLiteTutorials1/Models/Note.cs
@@ -6,14 +6,14 @@ using System.Text;
 using System.Threading.Tasks;
 using SQLite;
 
-namespace SQLiteTutorials1.Models
+namespace SQLiteTutorials1.Models // Namespace for the models in the SQLiteTutorials1 application
 {
-    public class Note
+    public class Note // Note class representing a note entity in the database
     {
         [PrimaryKey, AutoIncrement]  // This attribute marks the property as the primary key and enables auto-increment
         public int Id { get; set; }  // Unique identifier for each note
 
-        [MaxLength(250)]
+        [MaxLength(250)] // This attribute sets a maximum length for the text
         public string Text { get; set; } // The content of the note
         //public DateTime Date { get; set; }
 
